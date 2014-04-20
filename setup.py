86e9f3370a0d82141a1974eec8ab87c3c9d237ea
petitviolet <violethero0820@gmail.com>
2014-04-21 00:17:55 +0900

    modified platform

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pass-manager',
-    version='0.1.2',
+    version='1.0.0',
     author='petitviolet',
     author_email='violethero0820@gmail.com',
     packages=find_packages(),
@@ -11,7 +11,8 @@ setup(
     description = 'Simple CLI Password Manager',
     license = 'MIT',
     # scripts = ['src/pass_manager.py'],
-    platforms = ['POSIX', 'Windows', 'Mac OS X'],
+    platforms = ['Mac OS X'],
+    # platforms = ['POSIX', 'Windows', 'Mac OS X'],
     entry_points={
         'console_scripts': 'pass-manager = src.pass_manager:main'
     },
