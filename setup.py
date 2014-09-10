097db897b642148ca278e424cb33cca2c73dcd74
petitviolet <violethero0820@gmail.com>
2014-09-10 13:54:52 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pass-manager',
-    version='1.1.0',
+    version='1.2.0',
     author='petitviolet',
     author_email='violethero0820@gmail.com',
     packages=find_packages(),
